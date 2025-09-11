4b99639baffd2503957456d8ed187bbd11fd0892
Crist Evan <cristevanlamhotturnip@gmail.com>
2025-09-11 10:16:29 +0700

    data type

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strings"
+)
 
 func main()  {
 	fmt.Println("Hello World!")
+
+	// tipe data
+	var name string = "Madilog"
+	age := 80
+	const isAlive = true
+
+	fmt.Printf("Name: %v\n", name)
+	fmt.Printf("Age: %v\n", age)
+	fmt.Printf("Is Alive: %v\n", isAlive)
+
+	text := `Ada dua kemungkinan
+	1. Kemungkinan
+	2. Kemungkinan`
+
+	fmt.Println(text)
+
+	fmt.Println(len(name))
+	fmt.Println("Hello " + strings.ToUpper(name))
 }
