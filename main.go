9194558d6dae3bd41d261c3be029990905b153d5
Crist Evan <cristevanlamhotturnip@gmail.com>
2025-09-12 10:49:42 +0700

    type conversion

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -25,4 +26,32 @@ func main()  {
 
 	fmt.Println(len(name))
 	fmt.Println("Hello " + strings.ToUpper(name))
+
+	// type conversion
+	value1 := 32768.765
+	value2 := int(value1)
+
+	fmt.Println(value1)
+	fmt.Println(value2)
+
+	value3 := 100
+	value4 := strconv.Itoa(value3)
+
+	fmt.Printf("value3 is of type %T\n", value3)
+	fmt.Printf("value4 is of type %T\n", value4)
+
+	value5 := "100"
+	value6, _ := strconv.Atoi(value5)
+
+	fmt.Printf("value5 is of type %T\n", value5)
+	fmt.Printf("value6 is of type %T\n", value6)
+
+	value7 := true
+	value8 := strconv.FormatBool(value7)
+	value9, _ := strconv.ParseBool(value8)
+
+	fmt.Printf("value7 is of type %T\n", value7)
+	fmt.Printf("value8 is of type %T\n", value8)
+	fmt.Printf("value9 is of type %T\n", value9)
+
 }
